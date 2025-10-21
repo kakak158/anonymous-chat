ab8bd50ea3a9642ec95b5fe16638d9374e56477c
kakak158 <133575206+kakak158@users.noreply.github.com>
2025-10-21 15:12:15 +0100

    hiding my api key from evil ppl

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-  apiKey: "AIzaSyAqLDgxW9gdzrH-vnRALsDP11bbROUEEMU",
-  authDomain: "chat-app-ef85c.firebaseapp.com",
-  projectId: "chat-app-ef85c",
-  storageBucket: "chat-app-ef85c.firebasestorage.app",
-  messagingSenderId: "756197689277",
-  appId: "1:756197689277:web:4ce9ae9e84f169414374d4",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: "G-4CF1CKKEJK",
 };
 
